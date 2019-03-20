53675458a98aa07ab60f16f140dfc16489a86557
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-20 15:54:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
 end
